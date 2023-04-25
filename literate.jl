a0e7c860ba0fbe29b124205c71cb119505761d00
blegat <benoit.legat@gmail.com>
2023-04-25 21:05:03 +0000

    Deploying to gh-pages from  @ fbb68ce7c4a789b8dc46643be9c0dd508939b8a8 🚀

--- literate.jl
+++ literate.jl
@@ -1,22 +1,25 @@
 using Literate, NodeJS
 
 function gen_literate()
-    milp = joinpath(@__DIR__, "teaching", "milp")
+    milp = joinpath(@__DIR__, "milp")
     nbpath  = joinpath(@__DIR__, "generated", "notebooks")
     isdir(nbpath) || mkpath(nbpath)
 
     for script in readdir(milp)
+       if !endswith(script, ".jl")
+           continue
+       end
        # Generate markdown pages that Franklin will turn into html
        Literate.markdown(joinpath(milp, script), milp, documenter=false)
        # Generate annotated notebooks
        Literate.notebook(joinpath(milp, script), nbpath,
                          execute=false, documenter=false)
     end
-    JS_GHP = """
-        var ghpages = require('gh-pages');
-        ghpages.publish('generated/', function(err) {});
-        """
-    run(`$(nodejs_cmd()) -e $JS_GHP`)
+#    JS_GHP = """
+#        var ghpages = require('gh-pages');
+#        ghpages.publish('generated/', function(err) {});
+#        """
+#    run(`$(nodejs_cmd()) -e $JS_GHP`)
 end
 
 gen_literate()
