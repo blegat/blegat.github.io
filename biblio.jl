64fb2da7dfc6d9bb65a87f766ee5480c6f30231a
Benoît Legat <benoit.legat@gmail.com>
2024-01-20 18:57:52 +0100

    Update biblio

--- biblio.jl
+++ biblio.jl
@@ -65,14 +65,22 @@ end
 doi_url(doi) = "https://doi.org/" * doi
 arXiv_url(id) = "https://arxiv.org/abs/" * id
 
-function print_entry(io::IO, key::String)
+function print_entry(io::IO, key::String; kws...)
     if isnothing(BIB)
         load!()
     end
-    print_entry(io, BIB[key])
+    print_entry(io, BIB[key]; kws...)
 end
 
-function print_entry(io::IO, d)
+function cite(d)
+    return "[" * join([first(s.last) for s in d.authors]) * d.date.year[end-1:end] * "]"
+end
+
+function print_entry(io::IO, d; links = true, venue = true, doi = true, cite = false)
+    if cite
+        print(io, Biblio.cite(d))
+        print(io, ' ')
+    end
     print(io, _fix(Bibliography.xnames(d)))
     print(io, ". ")
     title = _fix(d.title)
@@ -82,41 +90,47 @@ function print_entry(io::IO, d)
     end
     tag(io, "strong", title)
     print(io, ".")
-    venue = Bibliography.xin(d)
-    if isempty(venue)
-        display(d)
-        error("Missing venue")
-    end
-    print(io, " ")
-    tag(io, "em", _fix(venue))
-    print(io, ", ")
-    doi = d.access.doi
-    if !isempty(doi)
+    if venue
+        venue = Bibliography.xin(d)
+        if isempty(venue)
+            display(d)
+            error("Missing venue")
+        end
         print(io, " ")
-        href(io, doi_url(doi), doi)
+        tag(io, "em", _fix(venue))
+        print(io, ", ")
     end
-    br_done = false
-    for name in KEYS
-        if haskey(d.fields, name)
-            if br_done
-                print(io, " ")
-            else
-                print(io, "<br/>")
-                br_done = true
-            end
-            value = d.fields[name]
-            if name == "code_doi"
-                url = doi_url(value)
-                content = "code " * value
-            else
-                if name == "arXiv"
-                    url = arXiv_url(value)
+    if doi
+        _doi = d.access.doi
+        if !isempty(_doi)
+            print(io, " ")
+            href(io, doi_url(_doi), _doi)
+        end
+    end
+    if links
+        br_done = false
+        for name in KEYS
+            if haskey(d.fields, name)
+                if br_done
+                    print(io, " ")
                 else
-                    url = value
+                    print(io, "<br/>")
+                    br_done = true
                 end
-                content = replace(name, "_" => " ")
+                value = d.fields[name]
+                if name == "code_doi"
+                    url = doi_url(value)
+                    content = "code " * value
+                else
+                    if name == "arXiv"
+                        url = arXiv_url(value)
+                    else
+                        url = value
+                    end
+                    content = replace(name, "_" => " ")
+                end
+                href(io, url, content)
             end
-            href(io, url, content)
         end
     end
     println(io)
