a5b230fb6ede309772e36e6aa69734b8e026607a
blegat <benoit.legat@gmail.com>
2024-11-02 16:54:59 +0000

    Deploying to gh-pages from  @ caed90565f92590f123ac3b0701a9e9b654e7175 🚀

--- ccir/lecture6.jl
+++ ccir/lecture6.jl
@@ -3,14 +3,10 @@
 using Graphs
 function build_graph()
     G = DiGraph(6)
-    w = Int[]
-    edge_ids = Dict{Pair{Int,Int},Int}()
-    cur_id = 0
+    w = Dict{Graphs.SimpleGraphs.SimpleEdge{Int},Int}()
     function add(from, to, weight)
-        cur_id += 1
-        edge_ids[from => to] = cur_id
         add_edge!(G, from, to)
-        push!(w, weight)
+        w[Edge(from => to)] = weight
     end
     add(1, 2, 3)
     add(1, 3, 3)
@@ -20,54 +16,34 @@ function build_graph()
     add(4, 5, 4)
     add(4, 6, 2)
     add(5, 6, 3)
-    return G, w, edge_ids
+    return G, w
 end
-G, w, edge_ids = build_graph()
+G, w = build_graph()
 using GraphPlot
-gplot(G, nodesize=0.1, nodelabel = 1:6, edgelabel = w)
+using Colors
 
-edge_ids
-
-w
-
-edge_ids[3 => 5]
-
-w[edge_ids[3 => 5]]
-
-Graphs.outneighbors(G, 1)
-
-[1 => to for to in Graphs.outneighbors(G, 1)]
-
-[edge_ids[1 => to] for to in Graphs.outneighbors(G, 1)]
-
-[x[edge_ids[1 => to]] for to in Graphs.outneighbors(G, 1)]
-
-sum([x[edge_ids[1 => to]] for to in Graphs.outneighbors(G, 1)])
-
-sum(x[edge_ids[1 => to]] for to in Graphs.outneighbors(G, 1))
+gplot(G, nodesize=0.1, nodelabel = 1:6, edgelabel = [w[e] for e in edges(G)], edgelabelsize = 2.0, edgelabelc = colorant"red")
 
 # # Max-Flow
 
 using JuMP
 import HiGHS
 model = Model(HiGHS.Optimizer)
-@variable(model, 0 <= x[i = 1:ne(G)] <= w[i])
-@objective(model, Max, sum(x[edge_ids[1 => to]] for to in Graphs.outneighbors(G, 1)))
+@variable(model, 0 <= flow[edge in edges(G)] <= w[edge])
+@objective(model, Max, sum(flow[Edge(1 => to)] for to in Graphs.outneighbors(G, 1)))
 optimize!(model)
 
-gplot(G, nodesize=0.1, nodelabel = 1:6, edgelabel = ["$(Int(value(x[i])))/$(w[i])" for i in 1:ne(G)])
+function viz()
+    gplot(G, nodesize=0.1, nodelabel = 1:6, edgelabel = ["$(Int(value(flow[edge])))/$(w[edge])" for edge in edges(G)], edgelabelc = colorant"red")
+end
+viz()
 
-value.(x)
+sum(x[edge_ids[3 => to]] for to in Graphs.outneighbors(G, 3))
+sum(x[edge_ids[from => 3]] for from in Graphs.inneighbors(G, 3))
 
-Graphs.outneighbors(G, 1)
-
-collect(2:(nv(G) - 1))
-
-edge_ids[2 => 3]
-
-edge_ids[2 => 4]
-
-sum(x[edge_ids[2 => to]] for to in outneighbors(G, 2))
+@constraint(model, sum(x[edge_ids[node => 3]] for node in inneighbors(G, 3)) == sum(x[edge_ids[3 => node]] for node in outneighbors(G, 3)))
+optimize!(model)
+viz()
 
 @constraint(
     model,
@@ -76,22 +52,14 @@ sum(x[edge_ids[2 => to]] for to in outneighbors(G, 2))
     ==
     sum(x[edge_ids[node => to]] for to in outneighbors(G, node)),
 )
-
-model[:x]
-
-println(model)
-
-@objective(model, Max,  sum(x[edge_ids[from => nv(G)]] for from in inneighbors(G, nv(G))))
-
 optimize!(model)
 solution_summary(model)
-
-gplot(G, nodesize=0.1, nodelabel = 1:6, edgelabel = ["$(Int(value(x[i])))/$(w[i])" for i in 1:ne(G)])
+viz()
 
 # # Min-Cut
 
 model = Model(HiGHS.Optimizer)
-@variable(model, cut[1:ne(G)], Bin)
+@variable(model, 0 <= cut[1:ne(G)] <= 1)
 
 @objective(model, Min, sum(w[e] * cut[e] for e in 1:ne(G)))
 
@@ -100,7 +68,7 @@ solution_summary(model)
 
 2cut[1] / 3cut[2] + 1
 
-gplot(G, nodesize=0.1, nodelabel = 1:6, edgelabel = value.(cut))
+gplot(G, nodesize=0.1, nodelabel = 1:6, edgelabel = value.(cut), edgelabelc = colorant"red")
 
 # | `side[from]` | `side[to]` | `side[to] - side[from]` |
 # |-----|-----|---------|
@@ -109,7 +77,7 @@ gplot(G, nodesize=0.1, nodelabel = 1:6, edgelabel = value.(cut))
 # |  1  |  1  |    0    |
 # |  0  |  0  |    0    |
 
-@variable(model, side[1:nv(G)], Bin)
+@variable(model, side[1:nv(G)])
 @constraint(model, side[1] == 0)
 @constraint(model, side[nv(G)] == 1)
 @constraint(
@@ -121,4 +89,4 @@ gplot(G, nodesize=0.1, nodelabel = 1:6, edgelabel = value.(cut))
 optimize!(model)
 solution_summary(model)
 
-gplot(G, nodesize=0.1, nodelabel = ["$(Int(value(side[node])))/$node" for node in 1:6], edgelabel = Int.(value.(cut)) .* w)
+gplot(G, nodesize=0.1, nodelabel = ["$(Int(value(side[node])))/$node" for node in 1:6], edgelabel = Int.(value.(cut)) .* w, edgelabelc = colorant"red")
